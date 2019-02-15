7b6c026d55bb883d149aab32836bb351f912d7ee
Uddergrief <Jaimestorres1982@icloud.com>
2019-02-15 09:55:39 -0800

    create block class/data structure

---
+++ test/blockchain.js
@@ -0,0 +1,10 @@
+class Block {
+  constructor(index, timestamp, nonce, prevBlockHash, hash, transactions){
+    this.index = index; // block number of chain
+    this.timestamp = timestamp; // when block created
+    this.transactions = transactions; // transaction stored in block
+    this.nonce = nonce; // added to block, makes hash output of block change
+    this.hash = hash; // compressed block info string
+    this.prevBlockHash = prevBlockHash // hash data from previous block
+  }
+}
